ff0fcefb66385ef700a63e9ae1ab364ba4f509a0
devmammadov <farid.me93@gmail.com>
2020-02-10 13:32:29 -0800

    v1

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { Grid } from "@material-ui/core";
+
+const App = () => {
+  return (
+    <Grid container>
+      <Grid item xs={12}></Grid>
+    </Grid>
+  )
+};
+
+export default App;
---
+++ src/components/layout/header/header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+export const Header = () => {
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        header
+      </Grid>
+    </Grid>
+  );
+};
